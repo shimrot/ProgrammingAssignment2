47bfd1cb339cda8eb5c6453487c40116fc0cd66f
Kris Davis <shimrot@gmail.com>
2014-09-18 19:19:40 -0500

    2nd Assignment done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This module defines a cacheable matrix object
+##    Use makeCacheMatrix to build the matrix with associated methods
+##    Use cacheSolve on the matrix to generate a inverse matrix
 
-## Write a short comment describing this function
 
+## makeCacheMatrix: generate a cacheable matrix
+## arguments: 
+##    x: regular matrix object (default is 1x1 matrix with NA value)
+## returns:
+##    cacheable matrix copy of x
 makeCacheMatrix <- function(x = matrix()) {
-
+    # copy of x remains in environment, so don't a new variable
+    xI <- NULL 
+    set <- function(m) {
+        x  <<- m  # value of x in parent environment of "set" is replaced
+        xI <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(mI) xI <<- mI
+    getInverse <- function() xI
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve: solve for inverse of cacheable matrix
+## arguments:
+##   x: cacheable matrix
+##   ...:  optional arguments passed onto R solve function
+## returns:
+##   inversion matrix
+## Assumptions: 
+##   * x matrix is invertable
+##   * x matrix is square
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    invX <- x$getInverse()
+    if(!is.null(invX)) {
+        message("getting cached data")
+        return(invX)
+    }
+    
+    
+    data <- x$get()
+    # create identity matrix (so optional args don't accidentally replace
+    # assumes x is square matrix
+    identM <- diag(, nrow(data))
+    
+    invX <- solve(data, identM, ...)
+    x$setInverse(invX)
+    invX
 }
---
+++ test_cachematrix.R
@@ -0,0 +1,55 @@
+# test cases
+
+runtests <-function()
+{    
+    message("Test 1")
+    a <- rbind(c(1,4,9),c(2,4,6),c(3,3,3))
+    message("a="); print(a)
+    b <- makeCacheMatrix(a)
+    c <- cacheSolve(b)
+    message("c="); print(c)
+    d <- round(a %*% c, 0.000001)  # get ident (round to remove small calc residuals)
+    message("ident="); print(d)
+    
+    message("Test 1 repeat with cache")
+    c <- cacheSolve(b)
+    message("c="); print(c)
+    d <- round(a %*% c, 0.000001)  # get ident (round to remove small calc residuals)
+    message("ident="); print(d)
+    
+    message("Test 2")
+    a <- rbind(c(1,2,3),c(4,4,3),c(9,6,3))
+    message("a="); print(a)
+    b <- makeCacheMatrix(a)
+    c <- cacheSolve(b)
+    message("c="); print(c)
+    d <- round(a %*% c, 0.000001)  # get ident (round to remove small calc residuals)
+    message("ident="); print(d)
+    
+    message("Test 2 repeat with cache")
+    c <- cacheSolve(b)
+    message("c="); print(c)
+    d <- round(a %*% c, 0.000001)  # get ident (round to remove small calc residuals)
+    message("ident="); print(d)
+    
+    message("Test 2 again with cache")
+    c <- cacheSolve(b)
+    message("c="); print(c)
+    d <- round(a %*% c, 0.000001)  # get ident (round to remove small calc residuals)
+    message("ident="); print(d)
+    
+    message("Test 3: matrix size 1")
+    a <- as.matrix(9)
+    message("a="); print(a)
+    b <- makeCacheMatrix(a)
+    c <- cacheSolve(b)
+    message("c="); print(c)
+    d <- round(a %*% c, 0.000001)  # get ident (round to remove small calc residuals)
+    message("ident="); print(d)
+    
+    message("Test 3 repeat with cache")
+    c <- cacheSolve(b)
+    message("c="); print(c)
+    d <- round(a %*% c, 0.000001)  # get ident (round to remove small calc residuals)
+    message("ident="); print(d)
+}
